abc3abbf929a29482f0fb81d42482a397b44534b
LewisJin <365932342@qq.com>
2016-12-09 22:51:18 +0800

    changes made

--- app/src/main/java/com/ouman/mordennews/NewsDetailActivity.java
+++ app/src/main/java/com/ouman/mordennews/NewsDetailActivity.java
@@ -2,6 +2,8 @@ package com.ouman.mordennews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class NewsDetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class NewsDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_news_detail);
+        toolbar.setTitle("New way to find");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NewsDetailActivity.this.finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/ouman/mordennews/adapters/HotRecyclerViewAdapter.java
+++ app/src/main/java/com/ouman/mordennews/adapters/HotRecyclerViewAdapter.java
@@ -1,6 +1,8 @@
 package com.ouman.mordennews.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.*;
 import android.view.LayoutInflater;
@@ -8,7 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.ouman.mordennews.MainActivity;
+import com.ouman.mordennews.NewsDetailActivity;
 import com.ouman.mordennews.R;
 import com.ouman.mordennews.models.HotNewsModel;
 
@@ -30,32 +35,52 @@ public class HotRecyclerViewAdapter extends RecyclerView.Adapter<HotRecyclerView
 
     public HotRecyclerViewAdapter.HotViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         View view = LayoutInflater.from(context).inflate(R.layout.hot_recyclerview_item, null);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("===>>click!!!!!");
+            }
+        });
+
         return new HotViewHolder(view);
     }
 
-    public void onBindViewHolder(HotRecyclerViewAdapter.HotViewHolder holder, int position){
+    public void onBindViewHolder(final HotRecyclerViewAdapter.HotViewHolder holder, final int position){
         String title = data.get(position).getTitle();
         String imageUrl = data.get(position).getImages();
         String date = data.get(position).getDate();
 
         holder.tv_title.setText(title);
         holder.tv_date.setText(date);
+        holder.hotCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(view.getContext(), "inside viewholder position = " +
+//                        String.valueOf(position), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(view.getContext(), NewsDetailActivity.class);
+                view.getContext().startActivity(intent);
+
+            }
+        });
+
     }
 
     public int getItemCount(){
         return data.size();
     }
 
-    class HotViewHolder extends ViewHolder{
+    public class HotViewHolder extends ViewHolder{
         TextView tv_title;
         TextView tv_date;
         ImageView news_image;
+        CardView hotCardView;
         public HotViewHolder(View view){
             super(view);
             tv_title = (TextView) view.findViewById(R.id.tv_title);
             tv_date = (TextView) view.findViewById(R.id.tv_date);
             news_image = (ImageView) view.findViewById(R.id.news_image);
-
+            hotCardView = (CardView) view.findViewById(R.id.hot_recyclerview_cardview);
         }
 
     }
